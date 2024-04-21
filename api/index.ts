48fa3a7df1866f4e3a47a51c7ff1e6b8dcf13f2a
semanurkekec <semanurkekec@gmail.com>
2024-04-21 16:26:06 +0300

    create: api calls and hook

---
+++ api/index.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosInstance } from "axios";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+const POKEMON_API = "https://api.pokemontcg.io/v2";
+
+const pokemonInstance = axios.create({ baseURL: POKEMON_API });
+
+interface GetConfig {
+  params?: Record<string, string>;
+  url?: string;
+}
+async function apiGet(instance: AxiosInstance, config: GetConfig) {
+  return await instance.request({ method: "GET", ...config });
+}
+
+async function pokemonGet(config: GetConfig) {
+  return await apiGet(pokemonInstance, config);
+}
+
+export function useGetPokemon<T = unknown>(config: GetConfig) {
+  // const queryFn = axios.request(options)
+  async function queryFn() {
+    return await pokemonGet(config)
+      .then((response) => ({
+        res: response.data as T,
+        ...response,
+      }))
+      .catch((error) => {
+        throw new Error("get pokemon error");
+      });
+  }
+  return useSuspenseQuery({ queryKey: ["pokemon", config], queryFn });
+}
